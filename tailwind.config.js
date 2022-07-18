800219846b892b9390024496f3c17e293edfd67f
Goody <101070150+Goody4444@users.noreply.github.com>
2022-07-18 15:04:39 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  darkMode: false,// or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+      'body': '#17171f',
+      'selected-text': 'A3A3FF',
+      'theme': '#3F3FFF', 
+      'nav':  '#404053', 
+      'secondary' : '#9191A4',
+      'badge': '#3F3F51',
+      'input-border': '#565666',
+      'input': '#2A2A35'
+       },
+       fontFamily: {
+        'poppins': ["'poppins'", ' sans-serif']
+       }
+    },
+  },
+  plugins: [],
+}
